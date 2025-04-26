0c12b769953da4dcb1050df544affff927599289
John <johnmuller0910@gmail.com>
2025-04-26 18:19:05 +0300

    Dashboard Layout

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,4 +1,14 @@
+import { Header } from "../../../components";
+
 const AllUsers = () => {
-  return <div>Users</div>;
+  return (
+    <main className={"dashboard wrapper"}>
+      <Header
+        title={"Trips Page"}
+        description={"Check out our current users in real time"}
+      />
+      user content
+    </main>
+  );
 };
 export default AllUsers;
--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,4 +1,17 @@
+import { Header } from "../../../components";
+
 const Dashboard = () => {
-  return <div>Dashboard page</div>;
+  const user = { name: "John" };
+  return (
+    <main className={"dashboard wrapper"}>
+      <Header
+        title={`Welcome ${user?.name ?? "Guest"} 👋`}
+        description={
+          "Track activity, trends and popular destinations in real time"
+        }
+      />
+      Dashbaord content
+    </main>
+  );
 };
 export default Dashboard;
